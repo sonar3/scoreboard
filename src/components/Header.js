5a29cf960d8368ecbbc83bcfb68c3717a4b3b39b
최철환 <hwanyhee@naver.com>
2019-07-20 18:02:06 +0900

    통계 컴포넌트 화면 작성 - emmet 구문 사용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Statistics} from "./Statistics";
 
 // 앞글자는 대문자, 반드시 react element를 리턴
 export const Header = ({title, totalPlayers}) => {
@@ -6,8 +7,8 @@ export const Header = ({title, totalPlayers}) => {
 
 	return (
 		<header className="header">
+			<Statistics/>
 			<h1 className="h1">{title}</h1>
-			<span className="stats">Players: {totalPlayers}</span>
 		</header>
 	)
 }
