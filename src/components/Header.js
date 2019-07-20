975634a5cc14843ea95d43b6c5b99f3ec52b1ea4
최철환 <hwanyhee@naver.com>
2019-07-20 18:15:31 +0900

    통계 컴포넌트 로직 작성

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Statistics} from "./Statistics";
 
 // 앞글자는 대문자, 반드시 react element를 리턴
-export const Header = ({title, totalPlayers}) => {
+export const Header = ({title, players}) => {
 	// 객체 해체 할당
 
 	return (
 		<header className="header">
-			<Statistics/>
+			<Statistics players={players} />
 			<h1 className="h1">{title}</h1>
 		</header>
 	)
