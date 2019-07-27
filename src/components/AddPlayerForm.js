475c10ffb94ae0c1e3935dca8021ee6b10a38b6f
최철환 <hwanyhee@naver.com>
2019-07-27 09:58:42 +0900

    AddPlayerForm -
    입력폼을 controlled 컴포넌트로 변환
    state로 모델을 정의하고 onChange이벤트로 모델값을 변경

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 export class AddPlayerForm extends React.Component {
+	state = {
+		value: ''
+	}
+
+	handleValueChange = (e) => {
+		console.log(e);
+		this.setState({value: e.target.value})
+	}
 
 	render() {
 		return (
 			<form className="form">
-				<input className="input" type="text" placeholder="enter a player's name"></input>
+				<input className="input" type="text" placeholder="enter a player's name"
+					value={this.state.value} onChange={this.handleValueChange}></input>
 				<input className="input" type="submit" value="Add Player"></input>
 			</form>
 		);
