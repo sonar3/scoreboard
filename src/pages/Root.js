27705a8baa2401c27ca23b230934b722fea12f92
최철환 <hwanyhee@naver.com>
2019-08-03 17:23:54 +0900

    공통메뉴 만들기 1

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,13 +3,14 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
 import {Heroes} from "./Heroes";
 import {Scoreboard} from "./Scoreboard";
+import {Menu} from "./Menu";
 
 export class Root extends React.Component {
 
 	render() {
 		return (
 			<BrowserRouter>
-				<p>공통메뉴</p>
+				<Menu></Menu>
 				<Switch>
 					<Route exact path="/" component={Home}></Route>
 					<Route path="/heroes" component={Heroes}></Route>
