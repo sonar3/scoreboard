b4a3c78914869c43e58cd00d1fccf5655cce4cfc
최철환 <hwanyhee@naver.com>
2019-08-03 16:55:31 +0900

    라우팅 설정

---
+++ src/pages/Heroes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Heroes extends React.Component {
+
+	render() {
+		return (
+			<div>
+				Heroes works.
+			</div>
+		);
+	}
+}
---
+++ src/pages/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Scoreboard extends React.Component {
+
+	render() {
+		return (
+			<div>
+				Scoreboard works.
+			</div>
+		);
+	}
+}
